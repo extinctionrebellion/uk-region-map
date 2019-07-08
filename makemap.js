0272c4d29d1fd7697515b897e09f60c113afc36c
C.J.Gutteridge <totl@srv02335.soton.ac.uk>
2019-07-08 17:26:58 +0100

    checkin

--- makemap.js
+++ makemap.js
@@ -1,30 +1,75 @@
 
 var map = L.map('map').setView([54.5, -2], 6);
-L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
     maxZoom: 21
 }).addTo(map);
 
+function onEachFeature(feature, layer, set, idfield) {
 
-function onEachFeature(feature, layer, set) {
-
-	var id = set+"-"+feature.properties.objectid;
-	if( ! document.getElementById(id) ) {
-		alert( "missing popup info for "+id );
-		return;
-	}
-	var popupContent = document.getElementById(id).innerHTML;
-//"<p>I started out as a GeoJSON " +
-//JSON.stringify( feature.properties )+"("+set+")"+
-			//feature.geometry.type + ", but now I'm a Leaflet vector!</p>";
-
-	if (feature.properties && feature.properties.popupContent) {
-		popupContent += feature.properties.popupContent;
-	}
-
-	layer.bindPopup(popupContent);
+/*
+  if( ! document.getElementById(id) ) {
+    alert( "missing popup info for "+id );
+    return;
+  }
+  var popupContent = document.getElementById(id).innerHTML;
+  layer.bindPopup(popupContent);
+*/
 }
 
-L.geoJSON(regions_england, {
-	onEachFeature: function(feature,layer) { onEachFeature(feature,layer,"region-england"); }
+var colours = {
+	"scotland":"red",
+	"northern-ireland":"orange",
+	"wales":"yellow",
+	"london":"purple",
+	"north-west":"blue",
+	"north-east":"cyan",
+	"west-midlands":"deeppink",
+	"east-midlands":"green",
+	"south-west":"magenta",
+	"south-east":"coral",
+	"east-of-england":"navy",
+	"yorkshire-and-the-humber":"brown"
+};
+function text_to_id(text) {
+  return text.toLowerCase().replace( /[^a-z ]/, '' ).replace( / /g, '-' );
+}
+for( var i=0;i<las.features.length;++i ) {
+    var feature = las.features[i];
+    feature.properties.country = text_to_id(country[feature.properties.lad19cd][2]);
+    if( feature.properties.country == "england" ) {
+      feature.properties.region = text_to_id(region[feature.properties.lad19cd][2]);
+    } else {
+      feature.properties.region = feature.properties.country;
+    }
+    if( county[feature.properties.lad19cd] ) { 
+      feature.properties.county = text_to_id(county[feature.properties.lad19cd][2]);
+    }
+    feature.properties.las = text_to_id(country[feature.properties.lad19cd][0]);
+}
+L.geoJSON(las, {
+  style: function (feature) {
+    return {
+      color: "#999999",
+      weight: 1,
+      fill: true,
+      fillColor: colours[feature.properties.region],
+      fillOpacity: 0.3,
+    };
+  },
+  onEachFeature: function(feature,layer) { 
+    layer.on({
+      mouseover: function(e) {
+console.log( feature.properties.las);
+        jQuery( '.area-info-section' ).hide();
+        jQuery( '#'+feature.properties.region ).show();
+        if( feature.properties['county'] ) {
+          jQuery( '#'+feature.properties.county ).show();
+        }
+        jQuery( '#'+feature.properties.las ).show();
+      },
+      mouseout: function(e) {
+      }
+    });
+  }
 }).addTo(map);
