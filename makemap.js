b2f6cc3422e34adb04ef36ae95b2cf1d428321e5
totl <totl@soton.ac.uk>
2019-12-30 14:45:08 +0000

    rejig data structure add region jump

--- makemap.js
+++ makemap.js
@@ -1,13 +1,10 @@
 jQuery(document).ready( function() {
 
+var uk;
 var regions = {};
+var counties = {};
+var nations = {};
 var laLookup = {};
-var feature_by_region = {};
-var feature_by_county = {};
-var marker_by_region = {};
-var marker_by_county = {};
-var bounds_by_region = {};
-var bounds_by_county = {};
 var FILL_DEFAULT = 0.5;
 var WEIGHT_DEFAULT = 2;
 var white_icon = make_icon('#fff');
@@ -30,7 +27,7 @@ function googleSheetToData( sheet, heading_row ) {
   var headings = [];
   for(var i=1;i<grid[heading_row].length;i++ ) {
     if( grid[heading_row][i] ) {
-      headings[i] = grid[heading_row][i].inputValue;
+      headings[i] = grid[heading_row][i].inputValue.toLowerCase();
     } else {
       headings[i] = 'col_'+i;
     }
@@ -69,6 +66,45 @@ function make_icon( colour ) {
   });
 }
 
+
+function featureCounty( county ) {
+  var c_layers = county.polygons;
+  // if LA is not in a county just reveal the current LA
+  for( var i=0; i<c_layers.length; i++ ) {
+    c_layers[i]._path.setAttribute('fill-opacity',FILL_DEFAULT/3);
+    c_layers[i]._path.setAttribute('stroke-width',0);
+  }
+  var c_markers = county.markers;
+  if( c_markers ) {
+    for( var i=0; i<c_markers.length; i++ ) {
+      c_markers[i].setIcon( white_icon );
+    }
+  }
+}
+
+function unfeatureCounty( county ) {
+  var c_layers = county.polygons;
+  for( var i=0; i<c_layers.length; i++ ) {
+    c_layers[i]._path.setAttribute('fill-opacity',FILL_DEFAULT);
+    c_layers[i]._path.setAttribute('stroke-width',WEIGHT_DEFAULT);
+  }
+  var c_markers = county.markers;
+  if( c_markers ) {
+    // if a marker has a muddle between region and county this could mess up the colour
+/*
+    if( regions[feature.properties.region] ) {
+      icon = regions[feature.properties.region].icon;
+    } else {
+      icon = plain_icon;
+    }
+*/
+    for( var i=0; i<c_markers.length; i++ ) {
+      var icon = c_markers[i].xr_usual_icon;
+      c_markers[i].setIcon( icon );
+    }
+  }
+}
+
 // make the map
 var map = L.map('map').setView([54.5, -2], 6);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
@@ -76,6 +112,8 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
     maxZoom: 21
 }).addTo(map);
 
+// data needs to be loaded in a specific order so the ajax calls chain rather
+// than all fire at the same time.
 loadData();
 
 function loadData() {
@@ -87,9 +125,12 @@ function loadData() {
     success: function( response ) {
       var records = googleSheetToData( response, 1 );
       for( var i=0; i<records.length; ++i ) {
-        var record = records[i];
-        record["icon"] = make_icon( record["Colour"] );
-        regions[record.ID] = record;
+        var region = records[i];
+        region["icon"] = make_icon( region["colour"] );
+        region.polygons = [];
+        region.markers = [];
+        region.bounds = null;
+        regions[region.id] = region;
       }
       loadData2();     
     }
@@ -107,28 +148,66 @@ function loadData2() {
 
       // convert la_map to an index
       for( var i=0; i<records.length; ++i ) {
-        laLookup[records[i]["Local Authority"]] = records[i];
+        laLookup[records[i]["local authority"]] = records[i];
       }
       
       for( var i=0;i<las.features.length;++i ) {
-          var feature = las.features[i];
-          var la = laLookup[feature.properties.lad19nm];
-          feature.properties.country = text_to_id(la["Nation"]);
-          feature.properties.region = text_to_id(la["XR Region"]);
-          feature.properties.county = text_to_id(la["County"]);
-          feature.properties.las = text_to_id(la["Local Authority"]);
-          
-          feature.properties.codes = [ feature.properties.region , feature.properties.las ];
-          if( feature.properties['county'] ) {
-            feature.properties.codes.push( feature.properties.county );
+        var feature = las.features[i];
+        var la = laLookup[feature.properties.lad19nm];
+
+        var nation_id = text_to_id(la["nation"]);
+        if( nation_id ) {
+          var nation = nations[ nation_id ];
+          if( !nation ) {
+            nation = {
+              label: la["nation"],
+              bounds: null,
+              markers: [],
+              polygons: [] 
+            };
+            nations[ nation_id ] = nation;
           }
-          if( feature.properties.country != feature.properties.region ) {
-            feature.properties.codes.push( feature.properties.country );
+          feature.properties.nation_id = nation_id;
+          feature.properties.nation = nation;
+          //nation.polygons.push( feature );
+        }
+        
+        var region_id = text_to_id(la["xr region"]);
+        if( region_id ) {
+          var region = regions[ region_id ];
+          if( !region ) {
+            region = {
+              label: la["xr region"],
+              bounds: null,
+              markers: [],
+              polygons: [] 
+            };
+            regions[ region_id ] = region;
+            console.log( "Unexpected region", la );
           }
-          feature_by_region[feature.properties.region] = [];
-          if( feature.properties.county ) {
-            feature_by_county[feature.properties.county] = [];
+          feature.properties.region_id = region_id;
+          feature.properties.region = region;
+          //region.polygons.push( feature );
+        } 
+
+        var county_id = text_to_id(la["county"]);
+        if( county_id ) {
+          var county = counties[ county_id ];
+          if( !county ) {
+            county = {
+              label: la["county"],
+              bounds: null,
+              markers: [],
+              polygons: []
+            };
+            counties[ county_id ] = county;
           }
+          feature.properties.county_id = county_id;
+          feature.properties.county = county;
+          //county.polygons.push( feature );
+        }
+
+        feature.properties.la = la;
       }
       loadData3();     
     }
@@ -138,96 +217,67 @@ function loadData2() {
 
 function loadData3() {
   // this data is stored locally so we don't have the same async issue as with the jquery calls
+  uk = {"label":"United Kingdom"};
   
   L.geoJSON(las, {
     style: function (feature) {
-      var colour = "#888888";
-      if( regions[feature.properties.region] )  { 
-        colour = regions[feature.properties.region].Colour;
-      }
       return {
-        color: colour,
+        color: feature.properties.region.colour,
         weight: WEIGHT_DEFAULT,
         opacity: 0.3,
         fill: true,
-        fillColor: colour,
+        fillColor: feature.properties.region.colour,
         fillOpacity: FILL_DEFAULT
       };
     },
     onEachFeature: function(feature,layer) { 
-      feature_by_region[feature.properties.region].push( layer );
-      if( bounds_by_region[feature.properties.region] ) {
-        bounds_by_region[feature.properties.region].extend( layer.getBounds() );
+
+      if( uk.bounds ) {
+        uk.bounds.extend( layer.getBounds() );
       } else {
-        bounds_by_region[feature.properties.region] = layer.getBounds();
+        uk.bounds = layer.getBounds();
       }
-      if( feature.properties.county ) {
-        feature_by_county[feature.properties.county].push( layer );
-        if( bounds_by_county[feature.properties.county] ) {
-          bounds_by_county[feature.properties.county].extend( layer.getBounds() );
+
+      var nation = feature.properties.nation;
+      if( nation ) {
+        nation.polygons.push( layer );
+        if( nation.bounds ) {
+          nation.bounds.extend( layer.getBounds() );
         } else {
-          bounds_by_county[feature.properties.county] = layer.getBounds();
+          nation.bounds = layer.getBounds();
         }
       }
   
-      layer.on({
-        mouseover: function(e) {
-          var c_layers = feature_by_county[feature.properties.county];
-          // if LA is not in a county just reveal the current LA
-          if( !c_layers ) { c_layers = [ layer ]; }
-          for( var i=0; i<c_layers.length; i++ ) {
-            c_layers[i]._path.setAttribute('fill-opacity',FILL_DEFAULT/3);
-            c_layers[i]._path.setAttribute('stroke-width',0);
-          }
-          var c_markers = marker_by_county[feature.properties.county];
-          if( c_markers ) {
-            for( var i=0; i<c_markers.length; i++ ) {
-              c_markers[i].setIcon( white_icon );
-            }
-          }
-          jQuery( '.area-info-section' ).hide();
-          for( var i=0;i<feature.properties.codes.length;++i ) { jQuery( '#'+feature.properties.codes[i] ).show(); } 
-          jQuery( '#area-debug' ).html( feature.properties.codes.join( ', ') );
-        },
-        mouseout: function(e) {
-          var c_layers = feature_by_county[feature.properties.county];
-          if( !c_layers ) { c_layers = [ layer ]; }
-          for( var i=0; i<c_layers.length; i++ ) {
-            c_layers[i]._path.setAttribute('fill-opacity',FILL_DEFAULT);
-            c_layers[i]._path.setAttribute('stroke-width',WEIGHT_DEFAULT);
-          }
-          var c_markers = marker_by_county[feature.properties.county];
-          if( c_markers ) {
-            // if a marker has a muddle between region and county this could mess up the colour
-            if( regions[feature.properties.region] ) {
-              icon = regions[feature.properties.region].icon;
-            } else {
-              icon = plain_icon;
-            }
-            for( var i=0; i<c_markers.length; i++ ) {
-              c_markers[i].setIcon( icon );
-            }
-          }
-          jQuery( '#area-debug' ).html( '' );
-        }
-      });
-      var popup_html = "<div>";
-      for( var i=0;i<feature.properties.codes.length;++i ) { 
-        var id = feature.properties.codes[i];
-        if( document.getElementById(id) ) {
-          popup_html += jQuery( '#'+id).html();
+      var region = feature.properties.region;
+      if( region ) {
+        region.polygons.push( layer );
+        if( region.bounds ) {
+          region.bounds.extend( layer.getBounds() );
+        } else {
+          region.bounds = layer.getBounds();
         }
       }
-      popup_html += "</div>";
-      layer.bindPopup(popup_html);
-     
-       
-  /*
-  if( ! document.getElementById(id) ) {
-    alert( "missing popup info for "+id );
-    return;
-  }
-*/
+
+      var county = feature.properties.county;
+      if( county ) {
+        county.polygons.push( layer );
+        if( county.bounds ) {
+          county.bounds.extend( layer.getBounds() );
+        } else {
+          county.bounds = layer.getBounds();
+        }
+      }
+  
+      if( county ) {
+        layer.on({
+          mouseover: function(e) { featureCounty( this ) }.bind( county ),
+           mouseout: function(e) { unfeatureCounty( this ) }.bind( county )
+        });
+      }
+          //jQuery( '.area-info-section' ).hide();
+//          for( var i=0;i<feature.properties.codes.length;++i ) { jQuery( '#'+feature.properties.codes[i] ).show(); } 
+//          jQuery( '#area-debug' ).html( feature.properties.codes.join( ', ') );
+
     }
   }).addTo(map);
   
@@ -243,103 +293,133 @@ function loadData4() {
       var records = googleSheetToData( response, 3 );
       for( var i=0;i<records.length;++i ) {
         var record = records[i];
-        var country;
+        var nation;
         var region;
         var county;
-        if( record["Country"] ) {
-          country = text_to_id(record["Country"]);
-        }
-        if( record["XR Region"] ) {
-          region = text_to_id(record["XR Region"]);
-        }
-        if( record["County"] ) {
-          county = text_to_id(record["County"]);
-        }
-        if( record["LatLong"] ) {
-/*
-          var colour = "#888888";
-          if( regions[region] )  { 
-            colour = regions[region].Colour;
-          }
-/*
-          var marker = L.circle(ll,{
-            color: '#000',
-            weight: 1,
-            fillColor: colour,
-            fillOpacity: 0.9,
-            radius: 1000
-          }).addTo(map);  
-*/
-          var ll = record["LatLong"].split( /\s*,\s*/ );
+
+        if( record["nation"] )    { nation = nations[ text_to_id(record["nation"])]; }
+        if( record["xr region"] ) { region = regions[ text_to_id(record["xr region"])]; }
+        if( record["county"] )    { county = counties[text_to_id(record["county"])]; }
+
+        if( record["latlong"] ) {
+
+          var ll = record["latlong"].split( /\s*,\s*/ );
           var icon;
-          if( regions[region] ) {
-            icon = regions[region].icon;
+          if( region ) {
+            icon = region.icon;
           } else {
             icon = plain_icon;
-console.log(region,regions);
           }
           var marker = L.marker(ll,{ icon: icon } );
+          marker.xr_usual_icon = icon;
+          marker.xr_record = record;
           marker.addTo(map);
+          if( nation ) { nation.markers.push( marker ); }
+          if( region ) { region.markers.push( marker ); }
+          if( county ) { county.markers.push( marker ); }
             
-
           var popup_html = "<div>";
-          popup_html += "<h2>"+record["Name"]+"</h2>";
-          if( record["Country"] ) {
-            popup_html += "<div>Country: "+record["Country"]+"</div>";
+          popup_html += "<h2>"+record["name"]+"</h2>";
+          if( record["nation"] ) {
+            popup_html += "<div>Nation: "+record["nation"]+"</div>";
           }
-          if( record["XR Region"] ) {
-            popup_html += "<div>Region affiliation: "+record["XR Region"]+"</div>";
+          if( record["xr region"] ) {
+            popup_html += "<div>Region affiliation: "+record["xr region"]+"</div>";
           }
-          if( record["County"] ) {
-            popup_html += "<div>County affiliation: "+record["County"]+"</div>";
+          if( record["county"] ) {
+            popup_html += "<div>County affiliation: "+record["county"]+"</div>";
           }
-          if( record["Page"] ) {
-            popup_html += "<div><a href='"+record["Page"]+"'>Facebook Page</a></div>";
+          if( record["page"] ) {
+            popup_html += "<div><a href='"+record["page"]+"'>Facebook Page</a></div>";
           }
-          if( record["Group"] ) {
-            popup_html += "<div><a href='"+record["Group"]+"'>Facebook Group</a></div>";
+          if( record["group"] ) {
+            popup_html += "<div><a href='"+record["group"]+"'>Facebook Group</a></div>";
           }
-          if( record["Twitter"] ) {
-            popup_html += "<div><a href='"+record["Twitter"]+"'>Twitter</a></div>";
+          if( record["twitter"] ) {
+            popup_html += "<div><a href='"+record["twitter"]+"'>Twitter</a></div>";
           }
-          if( record["Instagram"] ) {
-            popup_html += "<div><a href='"+record["Instagram"]+"'>Instagram</a></div>";
+          if( record["instagram"] ) {
+            popup_html += "<div><a href='"+record["instagram"]+"'>Instagram</a></div>";
           }
-          if( record["Website"] ) {
-            popup_html += "<div><a href='"+record["Website"]+"'>Website</a></div>";
+          if( record["website"] ) {
+            popup_html += "<div><a href='"+record["website"]+"'>Website</a></div>";
           }
-          if( record["Youtube"] ) {
-            popup_html += "<div><a href='"+record["Youtube"]+"'>Youtube</a></div>";
+          if( record["youtube"] ) {
+            popup_html += "<div><a href='"+record["youtube"]+"'>Youtube</a></div>";
           }
-          if( record["Action Network"] ) {
-            popup_html += "<div><a href='"+record["Action Network"]+"'>Action Network</a></div>";
+          if( record["action network"] ) {
+            popup_html += "<div><a href='"+record["action network"]+"'>Action Network</a></div>";
           }
-          if( record["Email"] ) {
-            popup_html += "<div><a href='mailto:"+record["Email"]+"'>"+record["Email"]+"</a></div>";
+          if( record["email"] ) {
+            popup_html += "<div><a href='mailto:"+record["email"]+"'>"+record["email"]+"</a></div>";
           }
           popup_html += "</div>";
           marker.bindPopup(popup_html);
-  
-          if( record["Country"] ) {
-            country = text_to_id(record["Country"]);
-            if( !marker_by_country[ country ] ) { marker_by_country[ country ]=[]; }
-            marker_by_country[ country ].push( marker );
+
+          if( record["county"] ) {
+          //  var county = text_to_id(record["county"]);
+            marker.on({
+              mouseover: function(e) { featureCounty( this ); }.bind(county),
+               mouseout: function(e) { unfeatureCounty( this ); }.bind(county)
+            });
           }
-          if( record["XR Region"] ) {
-            region = text_to_id(record["XR Region"]);
-            if( !marker_by_region[ region ] ) { marker_by_region[ region ]=[]; }
-            marker_by_region[ region ].push( marker );
-          }
-          if( record["County"] ) {
-            county = text_to_id(record["County"]);
-            if( !marker_by_county[ county ] ) { marker_by_county[ county ]=[]; }
-            marker_by_county[ county ].push( marker );
-          }
-  
         }
       }  
+      addQuickJumps();
     }
   });
+}
+
+var zoom_to = {};
+var select = 'uk';
+function addQuickJumps() {
+  var nation_ids = Object.keys(nations).sort(); 
+  var region_ids = Object.keys(regions).sort(); 
+  var county_ids = Object.keys(counties).sort(); 
+  var list = [];
+  zoom_to['uk'] = uk;
+  list.push( 'uk' );
+  for( var i=0; i<nation_ids.length; ++i ) {
+    var id = nation_ids[i];
+    zoom_to[id] = nations[id];
+    list.push( id );
+  }
+  for( var i=0; i<region_ids.length; ++i ) {
+    var id = region_ids[i];
+    zoom_to[id] = regions[id];
+    list.push( id );
+  }
+  for( var i=0; i<county_ids.length; ++i ) {
+    var id = county_ids[i];
+    zoom_to[id] = counties[id];
+    list.push( id );
+  }
+  select = jQuery( '<select></select>' );
+  for( var i=0; i<list.length; i++ ) {
+    var option = jQuery('<option></option>');
+    option.attr( 'value',  list[i] );
+    option.text( zoom_to[list[i]].label );
+    select.append( option );
+  }
+  select.change(function(){
+    var id = select.val();
+    window.location.hash = '#'+id;
+  });
+  jQuery( '#controls' ).append( select );
+
+  $(window).on('hashchange', update_from_hash );
+
+  update_from_hash();
+}
+
+function update_from_hash() {
+  var hash = window.location.hash.replace( /^#/, '' );
+  if( !hash ) { return; }
+
+  if( zoom_to[hash] && zoom_to[hash]['bounds']) {
+    map.fitBounds( zoom_to[hash].bounds );
+    select.val(hash);
+  }
 }
 
 }); //end of ready
